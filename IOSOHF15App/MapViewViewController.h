d2de316ca2625eee5b1ce9f7c2d175b31041bcbf
Thomas Kekeisen <thomas@thomaskekeisen.de>
2015-09-26 12:56:02 +0200

    Added the map

---
+++ IOSOHF15App/MapViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewViewController.h
+//  IOSOHF15App
+//
+//  Created by Thomas Kekeisen on 26/09/15.
+//  Copyright © 2015 Socialbit GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewViewController : UIViewController
+
+@end
---
+++ IOSOHF15App/MapViewViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewViewController.m
+//  IOSOHF15App
+//
+//  Created by Thomas Kekeisen on 26/09/15.
+//  Copyright © 2015 Socialbit GmbH. All rights reserved.
+//
+
+#import "MapViewViewController.h"
+
+@interface MapViewViewController ()
+
+@end
+
+@implementation MapViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
